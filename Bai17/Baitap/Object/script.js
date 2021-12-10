b0ee875b2d1ae7133ad0be11ca585e6ae1cf26ba
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2021-12-10 14:14:18 +0700

    bai17

--- Bai17/Baitap/Object/script.js
+++ Bai17/Baitap/Object/script.js
@@ -8,6 +8,7 @@
     this.left = left;
     this.size = size;
     this.speed = speed;
+    this.status = "MoveRight";
   
     this.getHeroElement = function(){
       return '<img width="'+ this.size + '"' +
@@ -20,17 +21,51 @@
       this.left += this.speed;
       console.log('ok: ' + this.left);
     }
+    this.moveBot = function(){
+      this.top += this.speed;
+      console.log('ok: ' + this.top);
+    }
+    this.moveLeft = function(){
+      this.left -= this.speed;
+      console.log('ok: ' + this.left);
+    }
+    this.moveTop = function(){
+      this.top -= this.speed;
+      console.log('ok: ' + this.top);
+    }
   
   }
   
-  var hero = new Hero('pikachu.png', 20, 30, 200,50);
+  var hero = new Hero('pikachu.png', 20, 30, 100,50);
   
   function start(){
-    if(hero.left < window.innerWidth - hero.size){
-      hero.moveRight();
+    if (hero.status == "MoveRight") {
+      if(hero.left < window.innerWidth - hero.size*2){
+        hero.moveRight();
+      } else {
+        hero.status="MoveBot"
+      }
+    } else if (hero.status == "MoveBot") {
+      if(hero.top < window.innerHeight - hero.size*2){
+        hero.moveBot();
+      } else {
+        hero.status="MoveLeft"
+      }
+    } else if (hero.status == "MoveLeft") {
+      if(hero.left > hero.size){
+        hero.moveLeft();
+      } else {
+        hero.status="MoveTop"
+      }
+    } else if (hero.status =="MoveTop") {
+      if(hero.top > hero.size){
+        hero.moveTop();
+      } else {
+        hero.status="MoveRight"
+      }
     }
     document.getElementById('game').innerHTML = hero.getHeroElement();
-    setTimeout(start, 500)
+    setTimeout(start, 100)
   }
   
   start();
