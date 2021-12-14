ea074773fcb41a4a14ab3662bfc4bab0d920f40f
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2021-12-14 15:00:53 +0700

    MSG

---
+++ Angular/Bai6/sign-in/src/app/form/form.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  public formgroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
