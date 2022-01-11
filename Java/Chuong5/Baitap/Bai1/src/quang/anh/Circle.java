9a8c4d66c1799244d1d728b32e7d7b3e465c46f9
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2022-01-11 11:20:23 +0700

    asdasd

---
+++ Java/Chuong5/Baitap/Bai1/src/quang/anh/Circle.java
@@ -0,0 +1,23 @@
+package quang.anh;
+
+public class Circle {
+    private double radius;
+    private String color;
+
+    public Circle() {
+        this.radius = 1.0;
+        this.color="red";
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ Java/Chuong5/Baitap/Bai1/src/quang/anh/Main.java
@@ -0,0 +1,9 @@
+package quang.anh;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getColor());
+    }
+}
