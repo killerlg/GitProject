e37f6c83b325b88c26834642bd089b81d8b30e09
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2022-01-11 15:36:32 +0700

    asdasd

---
+++ Java/Chuong6/Baitap/Bai3/src/MovablePoint.java
@@ -0,0 +1,49 @@
+public class MovablePoint extends Point{
+    private float xSpeed;
+    private float ySpeed;
+
+    public MovablePoint() {
+        this.xSpeed = 0.0f;
+        this.ySpeed = 0.0f;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void move() {
+        this.setX(this.getX()+this.xSpeed);
+        this.setY(this.getY()+this.ySpeed);
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+}
