ca4ac4a0c59f764bc44a822dd282fb1efa8314e1
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2022-01-11 11:22:54 +0700

    asdasd

---
+++ Java/Chuong5/Baitap/Bai2/src/quang/anh/Main.java
@@ -0,0 +1,9 @@
+package quang.anh;
+
+public class Main {
+    public static void main(String[] args) {
+        Student std = new Student();
+        std.setClasses("10A3");
+        std.setName("Quang Anh");
+    }
+}
---
+++ Java/Chuong5/Baitap/Bai2/src/quang/anh/Student.java
@@ -0,0 +1,17 @@
+package quang.anh;
+
+public class Student {
+    private String name;
+    private String classes;
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
