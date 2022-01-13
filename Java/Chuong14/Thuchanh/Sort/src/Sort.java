f8191f37cc55cd446d8416f8ffc574573e982f97
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2022-01-13 11:26:18 +0700

    as

---
+++ Java/Chuong14/Thuchanh/Sort/src/Sort.java
@@ -0,0 +1,73 @@
+public class Sort {
+    public static void main(String[] args) {
+
+    }
+    public static void bubbleSort(int[] list) {
+        boolean needNextPass = true;
+
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            /* Array may be sorted and next pass not needed */
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    /* Swap list[i] with list[i + 1] */
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true; /* Next pass still needed */
+                }
+            }
+        }
+    }
+
+    public static void bubbleSortByStep(int[] list) {
+        boolean needNextPass = true;
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    /* Swap list[i] with list[i + 1] */
+                    System.out.println("Swap " + list[i] + " with " + list[i + 1]);
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true; /* Next pass still needed */
+                }
+            }
+            /* Array may be sorted and next pass not needed */
+            if (needNextPass == false) {
+                System.out.println("Array may be sorted and next pass not needed");
+                break;
+            }
+            /* Show the list after sort */
+            System.out.print("List after the  " + k + "' sort: ");
+            for (int j = 0; j < list.length; j++) {
+                System.out.print(list[j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void selectionSort(double[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            /* Find the minimum in the list[i..list.length-1] */
+            double currentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            /* Swap list[i] with list[currentMinIndex] if necessary */
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
+}
