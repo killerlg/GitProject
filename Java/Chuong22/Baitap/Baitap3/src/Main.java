59422ec6a0711275f58b266cefb1fffbed0d98fa
Your NameQuangAnh <phuonganhdo0311@gmail.com>
2022-01-25 16:37:20 +0700

    ád

--- Java/Chuong22/Baitap/Baitap3/src/Main.java
+++ Java/Chuong22/Baitap/Baitap3/src/Main.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.Set;
 
 public class Main {
-    CollectionOperations co = new CollectionUtilsAdapter();
-    Client client = new Client(co);
-    List<Integer> sourceList = Arrays.asList(0, 1, 2, 3, 4, 5);
-    Set<Integer> targetSet = new HashSet<>(sourceList);
-    client.printMaxValue(targetSet);
+    public static void main(String[] args) {
+        CollectionOperations co = new CollectionUtilsAdapter();
+        Client client = new Client(co);
+        List<Integer> sourceList = Arrays.asList(0, 1, 2, 3, 4, 5);
+        Set<Integer> targetSet = new HashSet<>(sourceList);
+        client.printMaxValue(targetSet);
+    }
 }
